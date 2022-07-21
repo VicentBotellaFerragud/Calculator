65ea4c1da29332302293ae1f8c1b795516951fe5
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-07-21 22:58:26 +0200

    JS comments and title updated, content formatted

--- script.js
+++ script.js
@@ -5,42 +5,60 @@ let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 /**
- * This map function applies an "addEventListener" function to each element of the "buttons" collection.
+ * This map function/method applies an "addEventListener" function/method to each element of the "buttons" collection.
  */
 buttons.map(button => {
+
     //This adds an EventListener to each of the elements (buttons) of the "buttons" array. 
     button.addEventListener('click', (e) => {
+
         //This takes the inner text (the content) of the button pressed by the user and...
         switch (e.target.innerText) {
+
             //If the pressed button is the "C" button...
             case 'C':
+
                 //The "display" div is emptied.
                 display.innerText = '';
+
                 //And then the function stops.
                 break;
+
             //If the pressed button is the "=" button...
             case '=':
+
                 //The content previosuly added is evaluated (added, subtracted, multiplied or divided depending on the operator 
                 //introduced by the user). The eval() function evaluates the introduced content as JavaScript code and executes it.                                                   
                 try {
                     display.innerText = eval(display.innerText);
-                //If the user types in invalid mathematic operations, "Error" will be displayed in the "display" div.
+
+                //If the user types in invalid mathematic operations, "Error" is displayed in the "display" div.
                 } catch {
                     display.innerText = "Error";
                 }
+
                 //And then the function stops.
                 break;
+
             //If the pressed button is the "←" button...
             case '←':
+
+                //And there is something in the "display" div...
                 if (display.innerText) {
-                    //The last character of the "display" string is removed. But only IF there is already something in the "display" div.
+
+                    //The last character of the "display" string is removed.
                     display.innerText = display.innerText.slice(0, -1);
                 }
+
                 //And then the function stops.
                 break;
-            //Adds by default the inner text of the button (its content) to the inner content of the "display" div.
+
+            //This adds by default the inner text of the button (its content) to the inner content of the "display" div.
             default:
                 display.innerText += e.target.innerText;
+
         }
+
     });
+
 });
